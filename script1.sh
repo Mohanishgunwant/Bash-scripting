331b07fb342e5349c44b8c39925936b50d7d422b
“mohanish.gunwant” <mohanish.gunwant@dal.ca>
2023-05-07 21:30:31 -0400

    comm1

---
+++ script1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+read -p "What is your name? " name
+if [[ -z ${name} ]]        #True if the length of the string is zero
+then
+     echo "Please enter your name"
+ else
+     echo "Welcome ${name}"
+ fi
+
+
+
