fd8aef995c5953ce7eb8fe1b0e7b68013dd09a7e
“mohanish.gunwant” <mohanish.gunwant@dal.ca>
2023-05-07 21:35:42 -0400

    script1.sh

--- script1.sh
+++ script1.sh
@@ -3,7 +3,7 @@
 read -p "What is your name? " name
 if [[ -z ${name} ]]        #True if the length of the string is zero
 then
-     echo "Please enter your name"
+     echo "Please enter your name "
  else
      echo "Welcome ${name}"
  fi
