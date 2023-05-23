30ae4380ae8dc1abc33e0d98f41133d64317bd91
mohanishgunwant <mohanish.gunwant@dal.ca>
2023-05-23 18:19:42 -0400

    modified code

--- Determine-the-number.sh
+++ Determine-the-number.sh
@@ -9,3 +9,24 @@ else
     echo "the number you puched is 0"
 fi
 
+
+#To comment selected text in vscode use: ctrl+K+C in windows
+
+#Below is if else elif structure:
+# if condition
+#  then
+#  condition is zero (true - 0)
+#  execute all commands up to elif statement
+#  elif condition1
+#  condition1 is zero (true - 0)
+#  execute all commands up to elif statement
+#  elif condition2
+#  condition2 is zero (true - 0)
+#  execute all commands up to elif statement
+# Linux Shell Script Tutorial
+# http://www.freeos.com/guides/lsst/shellprog.htm (13 of 19) [17/08/2001 17.42.21]
+#  else
+#  None of the above condtion,condtion1,condtion2 are true (i.e.
+#  all of the above nonzero or false)
+#  execute all commands up to fi
+#  fi
